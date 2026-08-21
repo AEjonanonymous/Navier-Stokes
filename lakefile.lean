646e874084ce06404daa6834f75316e50ff8f182
Jonathan ƒ(n) Reed <jonathanalanreed1983@gmail.com>
2026-08-21 13:56:18 -0400

    Update lakefile.lean

--- lakefile.lean
+++ lakefile.lean
@@ -4,7 +4,10 @@ open Lake DSL
 package «NavierStokes» where
 
 require mathlib from git
-  "https://github.com/leanprover-community/mathlib4.git" @ "v4.33.0"
+  "https://github.com/leanprover-community/mathlib4.git" @ "v4.13.0"
+
+lean_lib «Challenge» where
+  srcDir := "."
 
 @[default_target]
 lean_lib «NavierStokes» where
