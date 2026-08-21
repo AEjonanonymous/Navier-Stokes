07f655ad3b678a5b0f74e9f041e5eae79300f784
Jonathan ƒ(n) Reed <jonathanalanreed1983@gmail.com>
2026-08-21 15:41:19 -0400

    Update Solution.lean

--- Solution.lean
+++ Solution.lean
@@ -1,14 +1,7 @@
 import Challenge
 
-theorem navier_stokes_geometric_exclusion (Ω : Set Point3D) (corner : Point3D) (u : VectorField3D) (p_field : ScalarField3D) (nu : ℝ) (lambda : ℝ)
-  (h_pde : FullNavierStokesPDE u p_field nu)
-  (h_balance : inertial_scaling lambda = viscous_scaling lambda)
-  (h_domain : IsRightAngleCornerDomain Ω)
-  (hx_pos : corner.1 > 0)
-  (h_noslip : NoSlipBoundary u corner) 
-  (h_smooth_bound : lambda > -1) :
-  ¬ (GlobalSmoothSolution u) := by
-  intro h_smooth
+theorem navier_stokes_geometric_exclusion := by
+  intro Ω corner u p_field nu lambda h_pde h_balance h_domain hx_pos h_noslip h_smooth_bound h_smooth
   have h_bal := pde_implies_scaling_balance u p_field nu h_pde lambda h_balance
   have h_grad_active := corner_domain_forces_gradient Ω corner h_domain hx_pos
   have h_shear_enforced := corner_shearing_requirement u corner h_noslip h_grad_active
